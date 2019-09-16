144279943a3dbfa703692e9b24397f080c65392c
Louise van der Lith <louisevanderlith@gmail.com>
2019-09-16 22:57:14 +0200

    Got comms loading again

--- controllers/comms/messages.go
+++ controllers/comms/messages.go
@@ -13,10 +13,8 @@ type Messages struct {
 }
 
 func (c *Messages) Get(ctx context.Requester) (int, interface{}) {
-	//c.Setup("messages", "Messages", true)
-
 	var result []interface{}
-	code, err := do.GET(ctx.GetMyToken(), &result, ctx.GetInstanceID(), "Comms.API", "message", "A10")
+	code, err := do.GET(ctx.GetMyToken(), &result, ctx.GetInstanceID(), "Comms.API", "messages", "A10")
 
 	if err != nil {
 		log.Println(err)
@@ -27,11 +25,9 @@ func (c *Messages) Get(ctx context.Requester) (int, interface{}) {
 }
 
 func (c *Messages) Search(ctx context.Requester) (int, interface{}) {
-	//c.Setup("messages", "Messages", true)
-
 	var result []interface{}
 	pagesize := ctx.FindParam("pagesize")
-	code, err := do.GET(ctx.GetMyToken(), &result, ctx.GetInstanceID(), "Comms.API", "message", pagesize)
+	code, err := do.GET(ctx.GetMyToken(), &result, ctx.GetInstanceID(), "Comms.API", "messages", pagesize)
 
 	if err != nil {
 		log.Println(err)
@@ -42,8 +38,6 @@ func (c *Messages) Search(ctx context.Requester) (int, interface{}) {
 }
 
 func (c *Messages) View(ctx context.Requester) (int, interface{}) {
-	//c.Setup("messageView", "View Message", false)
-
 	key, err := husk.ParseKey(ctx.FindParam("key"))
 
 	if err != nil {
@@ -51,7 +45,7 @@ func (c *Messages) View(ctx context.Requester) (int, interface{}) {
 	}
 
 	result := make(map[string]interface{})
-	code, err := do.GET(ctx.GetMyToken(), &result, ctx.GetInstanceID(), "Comms.API", "message", key.String())
+	code, err := do.GET(ctx.GetMyToken(), &result, ctx.GetInstanceID(), "Comms.API", "messages", key.String())
 
 	if err != nil {
 		log.Println(err)
